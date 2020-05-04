af3e22da2059a0bcbb9acee5f2d04c70c348a750
contactjvitconsulting <64465837+contactjvitconsulting@users.noreply.github.com>
2020-05-04 19:12:20 +0530

    Update bringdown.sh

--- hooks/predelete/bringdown.sh
+++ hooks/predelete/bringdown.sh
@@ -2,6 +2,7 @@
 gcloud container clusters get-credentials "$CLUSTER_NAME" --zone "$INSTANCE_ZONE"
 gcloud sql instances delete "$INSTANCE_NAME" --quiet
 gsutil rm -r gs://"$BUCKET_NAME"
+gcloud iam service-accounts delete "$FULL_SA_NAME"
 kubectl delete secret cloudsql-instance-creds
 kubectl delete secret cloudsql-db-credentials
 kubectl delete configmap connectionname
